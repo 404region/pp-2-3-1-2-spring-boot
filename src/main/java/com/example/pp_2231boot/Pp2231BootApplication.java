bbf3c75676f07e17fb5368e63edf82951eddd608
Ann S <404region@gmail.com>
2023-11-04 13:34:24 +0300

    Initial Commit

---
+++ src/main/java/com/example/pp_2231boot/Pp2231BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_2231boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp2231BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pp2231BootApplication.class, args);
+    }
+
+}
